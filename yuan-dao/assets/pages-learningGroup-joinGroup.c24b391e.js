162d44a80a1048c23b8c88640152009d9beb968d
XZJ <feiyun260@qq.com>
2022-11-19 17:47:35 +0800

    remove yuan-dao

--- yuan-dao/assets/pages-learningGroup-joinGroup.c24b391e.js
+++
@@ -1,1 +0,0 @@
-import{o as a,c as e,w as r,d as s,i as t}from"./index.57b29d31.js";import{_ as d}from"./plugin-vue_export-helper.21dcd24c.js";var o=d({data:()=>({}),methods:{}},[["render",function(d,o,n,u,i,l){const p=t;return a(),e(p,null,{default:r((()=>[s(" 加入小组 ")])),_:1})}]]);export{o as default};
